f4971a94400254d88665a7e00e8df7193010a439
Sashasssda <96662367+Sashasssda@users.noreply.github.com>
2025-02-14 12:51:13 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#Config бота
+
+CRYPTOTOKEN = '338464:AAftRTYBdYz3wZ75KD3HyLfMqgQKC8Iey8i' # брать у @send
+TOKEN = '7839247165:AAHjpC2KvvLKAWDbV-jHGOQd2u0eaSsRJeI' # токен брать у botfather
+DIRECTORY = 'kosSESSjj' # не трогать
+photo_path = 'Doxbin.png' # ссылка на фото
+ADMIN = 7005465879 # айди админа брать у getmyid
+SUPPORT = 'http://t.me/Deffersnoserbot' # ссылка на тех.поддержкуц
+API_ID = 26484445 # не трогать
+API_HASH = '1181da6642bfbd3bd7275754d3f16a30' # не трогать
